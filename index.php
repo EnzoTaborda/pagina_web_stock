01547348a4b0745716d906fc4bdc090fbbe8cf0f
Greensapo2612 <146991324+Greensapo2612@users.noreply.github.com>
2023-11-01 15:50:35 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>pagina web 0.1</title>
+    <h1>bienvenidos!</h1>
+    ingrese su usuario <br>
+    <a  href="">usuario</a><br><br>
+    sin usuario? registrese! <br>
+    <a href="">contraseña</a><br>
+    
+  </head>
+</html>
